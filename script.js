b374245502f03e9ce4d1c5ffbdcf2dad0614c466
mayurpat999 <mayureshpatil1893@gmail.com>
2025-01-11 11:51:05 +0530

    ..

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('purchaseOrderForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle form submission here
+    alert('Purchase Order submitted!');
+});
